18aee5217944842f895aa4ff89fbb7ee95689610
Olaleye Iyanuoluwa <71622861+Olaleye-arch@users.noreply.github.com>
2023-11-20 21:54:01 +0100

    Add files via upload

--- plugins/truecaller.js
+++ plugins/truecaller.js
@@ -1,17 +1,41 @@
-const {
-	Function,
-	truecaller,
-	isPublic
-} = require('../lib/');
+const { bot, truecaller, jidToNum, delTruecaller, getTruecaller } = require('../lib/index')
 
-Function({
-	pattern: 'true ?(.*)',
-	fromMe: isPublic,
-	desc: 'search number on truecaller',
-	type: 'search'
-}, async (message, match, client) => {
-	if (!match && !message.quoted) return await message.reply('_Enter the number you want to search_');
-	const number = message.quoted ? message.quoted.sender : match.replace(/[^0-9]/g, '')
-	const result = await truecaller(number, message.user_id)
-	return await message.reply(result);
-})
+bot(
+  {
+    pattern: 'truecaller ?(.*)',
+    fromMe: true,
+    desc: 'search number in truecaller',
+    type: 'search',
+  },
+  async (message, match) => {
+    match =
+      (message.mention[0] && jidToNum(message.mention[0])) ||
+      match ||
+      (message.reply_message && jidToNum(message.reply_message.jid))
+    if (!match) return await message.send(`*Example :* truecaller 919876543210`)
+    if (match === 'token') {
+      const token = await getTruecaller()
+      if (!token) return await message.send(`*Your not logined*`)
+      return await message.send(token, { quoted: message.quoted })
+    }
+    if (match === 'logout') {
+      await delTruecaller()
+      return await message.send(`Logged out from Truecaller.`)
+    }
+    const res = await truecaller.search(match)
+
+    if (res.message) {
+      return await message.send(res.message)
+    }
+    let msg = ''
+    if (res.name) msg += `*Name :* ${res.name}\n`
+    if (res.gender) msg += `*Gender :* ${res.gender}\n`
+    if (res.email) msg += `*Email :* ${res.email}\n`
+    msg += `*Type :* ${res.numberType}(${res.type})\n`
+    msg += `*Carrier :* ${res.carrier}\n`
+    msg += `*Number :* ${res.number}\n`
+    if (res.city) msg += `*City :* ${res.city}\n`
+    msg += `*DailingCode :* ${res.dialingCode}(${res.countryCode})`
+    await message.send(msg)
+  }
+)
